6a100cf70469919ae40cab7404bb6e7af33e2945
Vector05 <mathurraghav23@gmail.com>
2026-09-25 11:00:01 +0530

    Initial commit

---
+++ how_many.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+    
+    // 1. How Many?
+    
+    // input (n)
+    int n;
+    std::cin >> n;
+    
+    // output (n * 3)
+    std::cout << (n * 3);
+    
+    return 0;
+}
